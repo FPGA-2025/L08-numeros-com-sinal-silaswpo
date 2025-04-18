0418bfe35ca8d88546372af83a513a81a642026e
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-17 23:22:49 -0300

    Tentando corrigir o codigo

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -4,9 +4,28 @@ module numeros_com_sinal(
     input [7:0] entrada_unsigned_1,
     input [3:0] entrada_unsigned_2,
     input [1:0] codigo,
-    output reg [7:0] saida
+    output signed[7:0] saida
 );
 
-//insira seu codigo aqui
+/*always @(*) begin
+
+    case (codigo)
+        2'b00: saida = entrada_signed_1 + entrada_signed_2;
+        2'b01: saida = entrada_unsigned_1 + entrada_unsigned_2;
+        2'b10: saida = entrada_unsigned_1 + entrada_signed_1;
+        2'b11: saida = entrada_unsigned_1 + entrada_signed_2;
+        default: saida = 8'd0;
+    endcase
+
+end
+
+endmodule 
+*/
+
+assign saida =  (codigo == 2'b00) ? $signed(entrada_signed_1 + entrada_signed_2):
+                (codigo == 2'b01) ? $signed(entrada_unsigned_1 + entrada_unsigned_2):
+                (codigo == 2'b10) ? $signed(entrada_unsigned_1 + entrada_signed_1):
+                (codigo == 2'b11) ? $signed(entrada_unsigned_1 + entrada_signed_2):
+                8'sd0;
 
 endmodule
